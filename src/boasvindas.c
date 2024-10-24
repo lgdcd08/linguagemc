91e1dd81fe746a37cf249b8325bf7be9da3bdee3
luiz carvalho <lgdcd08@gmail.com>
2024-10-24 13:53:13 -0300

    Primeiros aquivos em C

---
+++ src/boasvindas.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    // adicionar um pequeno cabeçalho
+    printf("--------programa mensagem----------");
+    printf("\nSeja bem Vindo ao nosso programa\n");
+    printf("-----------fim----------------------");
+
+    return 0;
+}
---
+++ src/hello.c
@@ -0,0 +1,12 @@
+// Impotação da biblioteca padrão para o uso de teclado e monitor
+#include <stdio.h>
+
+int main(){
+// para exibir uma mensagem na tela iremos usar
+// o comando printf(exibir formatando
+printf( "Hello, World! \n");
+// informar a o compilador que o programa 
+// acabou
+return 0;
+
+}
